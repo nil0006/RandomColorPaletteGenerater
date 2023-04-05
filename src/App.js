ef72660d59a4aaf662b7658a36c700207d63e148
Niladri Karmakar <niladri.karmakar@ingredion.com>
2023-04-05 14:12:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+function genNewTemp(){
+  var url = "http://colormind.io/api/";
+var data = {
+	model : "default",
+	input : [[44,43,44],[90,83,82],"N","N","N"]
+}
 
+var http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+	if(http.readyState == 4 && http.status == 200) {
+		var palette = JSON.parse(http.responseText).result;
+    console.log(palette);
+	}
+}
+
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
+
+}
 function App() {
+  genNewTemp()
   return (
     <div className="App">
       <header className="App-header">
